5001f6b9675877d52f8abe3894fd80ab7317cdb9
Anirudh Rautela <anirudhrautela9906@gmail.com>
2026-04-06 12:21:52 +0530

    Splash screen done

--- src/modules/onboard/index.tsx
+++ src/modules/onboard/index.tsx
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { Colors, screenHeight, screenWidth } from '@utils/Constants';
+import { resetAndNavigate } from '@navigation/NavigationUtil';
 
 const Splash = () => {
-  return (
-    <View>
-      <Text>Splash</Text>
-    </View>
-  )
-}
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      resetAndNavigate('Splash');
+    }, 1000);
 
-export default Splash
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('@assets/images/logo_t.png')}
+        style={styles.image}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary,
+  },
+  image: {
+    width: screenWidth * 0.35,
+    height: screenHeight * 0.35,
+    resizeMode: 'contain',
+  },
+});
+export default Splash;
